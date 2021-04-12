63a80ebfc1bfc9ee7ac96c5a9fc6a36f0d29763e
Juliana Gomez <juliana@gomezjuliana.com>
2021-04-12 16:56:35 -0500

    change images for confirmation modal

--- packages/app-shell/src/Confirmation/getCopy.js
+++ packages/app-shell/src/Confirmation/getCopy.js
@@ -5,6 +5,7 @@ const getCopy = ({ planName, startedTrial, onlyUpdatedCardDetails }) => {
       label: 'Congrats! You are now starting your trial',
       description: 'You can now experience all Buffer has to offer. Have fun!',
       buttonCopy: SUCCESS_CTA,
+      imageUrl: 'https://buffer-ui.s3.amazonaws.com/illustration-celebrate.png'
     };
   }
 
@@ -14,12 +15,14 @@ const getCopy = ({ planName, startedTrial, onlyUpdatedCardDetails }) => {
       description:
         'Your change have gone through successfully. Start using your Free plan today.',
       buttonCopy: SUCCESS_CTA,
+      imageUrl: 'https://buffer-ui.s3.amazonaws.com/illustration-celebrate.png'
     };
   } else if (planName) {
     return {
       label: `Congrats! Welcome to the ${planName} plan`,
       description: `Your details have gone through successfully. Start using your ${planName} plan features.`,
       buttonCopy: SUCCESS_CTA,
+      imageUrl: 'https://buffer-ui.s3.amazonaws.com/illustration-celebrate.png'
     };
   }
 
@@ -28,6 +31,7 @@ const getCopy = ({ planName, startedTrial, onlyUpdatedCardDetails }) => {
     description:
       'Thank you, your billing details have gone through successfully.',
     buttonCopy: "That's great!",
+    imageUrl: 'https://buffer-ui.s3.amazonaws.com/illustration-highfive.png'
   };
 };
 
